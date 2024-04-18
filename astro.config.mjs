faff563d27f39ab2defcf9a14b851c28ad2a4ec3
Baynov Ivan <Jhon0@yandex.ru>
2024-04-18 10:55:21 +0800

    feat: config for github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://dfyz011.github.io/your-repository",
+	outDir: "./docs", // Output to 'docs' folder, which GitHub Pages can use
+	// base: '/your-repository/',  // Adjust the base path if necessary
+});
