94a1aeea5a7842f251b6c85facfbaed3c3320ede
Baynov Ivan <Jhon0@yandex.ru>
2024-04-18 17:01:01 +0800

    feat: configure first collection

---
+++ src/content/config.js
@@ -0,0 +1,16 @@
+import { z, defineCollection } from "astro:content";
+const filmsCollectioon = defineCollection({
+	type: "content",
+	schema: ({ image }) =>
+		z.object({
+			videoUrl: z.string(),
+			title: z.string(),
+			text: z.string(),
+			previewImgUrl: image(),
+			gifUrl: z.string(),
+		}),
+});
+
+export const collections = {
+	films: filmsCollectioon,
+};
