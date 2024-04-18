6dca0efd6ea30f90f63098c0c833954015282d79
Baynov Ivan <Jhon0@yandex.ru>
2024-04-18 18:22:05 +0800

    feat:add logic with deplot to repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://dfyz011.github.io/utro-zhizni",
+	site: "https://dfyz011.github.io",
 	outDir: "./docs", // Output to 'docs' folder, which GitHub Pages can use
 	base: "/utro-zhizni", // Adjust the base path if necessary
 	trailingSlash: "ignore",
