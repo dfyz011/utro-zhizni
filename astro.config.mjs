3b6beb4c0dca465111af667d72016fb3d98956c8
Baynov Ivan <Jhon0@yandex.ru>
2024-04-19 20:30:28 +0800

    feat: deploy to domain so change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	site: "https://dfyz011.github.io",
 	// outDir: "./docs", // Output to 'docs' folder, which GitHub Pages can use
-	base: "/utro-zhizni", // Adjust the base path if necessary
+	base: "/", // Adjust the base path if necessary
 	trailingSlash: "ignore",
 	build: {
 		// Example: Generate `page.html` instead of `page/index.html` during build.
